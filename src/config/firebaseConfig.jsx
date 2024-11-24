ecc08e15078b7a914650e40607b996e10a1eae5b
Shanmukha Rana Prathap M <102742410+shanmukh0504@users.noreply.github.com>
2024-11-24 11:52:39 +0530

    Update firebaseConfig.jsx

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
